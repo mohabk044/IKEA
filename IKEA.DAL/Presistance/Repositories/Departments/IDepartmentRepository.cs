d9595dbad0de26efa758f000526edd0084b8e16c
Mohab Khaled <mohabk044@gmail.com>
2025-03-06 06:50:44 +0200

    Part 06 DAL(Department Repository)

---
+++ IKEA.DAL/Presistance/Repositories/Departments/IDepartmentRepository.cs
@@ -0,0 +1,19 @@
+﻿using IKEA.DAL.Models.Departments;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IKEA.DAL.Presistance.Repositories.Departments
+{
+    internal interface IDepartmentRepository
+    {
+        IEnumerable<Department> GetAll(bool withAsNoTracking = true);
+        Department? GetById(int id);
+        int Add(Department entity);
+        int Update(Department entity);
+        int Delete(Department entity);
+
+    }
+}
