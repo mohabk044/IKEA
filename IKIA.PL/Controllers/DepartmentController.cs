06ded09c40757838d90c0b4af7e571244492ed34
Mohab Khaled <mohabk044@gmail.com>
2025-03-13 15:36:48 +0200

    Part 03 Department Controller

---
+++ IKIA.PL/Controllers/DepartmentController.cs
@@ -0,0 +1,21 @@
+﻿using IKEA.BLL.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IKIA.PL.Controllers
+{
+    //Inheritance: Department Controller is a Controller 
+    //composition: Department Controller has a IDepartmentservice
+    public class DepartmentController : Controller
+    {
+        private readonly IDepartmentService _departmentService;
+
+        public DepartmentController(IDepartmentService departmentService)
+        {
+            _departmentService = departmentService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
