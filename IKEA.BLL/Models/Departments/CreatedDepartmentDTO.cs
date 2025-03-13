be20e344c68f02469460ddf3a4f0a49104bba022
Mohab Khaled <mohabk044@gmail.com>
2025-03-14 00:46:17 +0200

    Part 05 Department Controller [Create]

--- IKEA.BLL/Models/Departments/CreatedDepartmentDTO.cs
+++ IKEA.BLL/Models/Departments/CreatedDepartmentDTO.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +10,14 @@ namespace IKEA.BLL.Models.Departments
 {
     public class CreatedDepartmentDTO
     {
-        public string Name { get; set; } = null!;
+
+        [Required(ErrorMessage = "Code is required")]
         public string Code { get; set; } = null!;
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; } = null!;
         public string? Description { get; set; }
+
+        [DisplayName("Date Of Creation")]
         public DateOnly CreationDate { get; set; }
 
     }
--- IKIA.PL/Controllers/DepartmentController.cs
+++ IKIA.PL/Controllers/DepartmentController.cs
@@ -1,5 +1,7 @@
-﻿using IKEA.BLL.Services;
+﻿using IKEA.BLL.Models.Departments;
+using IKEA.BLL.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.DotNet.Scaffolding.Shared.Messaging;
 
 namespace IKIA.PL.Controllers
 {
@@ -8,10 +10,15 @@ namespace IKIA.PL.Controllers
     public class DepartmentController : Controller
     {
         private readonly IDepartmentService _departmentService;
+        private readonly IWebHostEnvironment _environment;
 
-        public DepartmentController(IDepartmentService departmentService)
+        public ILogger _Logger { get; }
+                       
+        public DepartmentController(IDepartmentService departmentService, ILogger<DepartmentController> Logger, IWebHostEnvironment environment)
         {
             _departmentService = departmentService;
+            _Logger = Logger;
+            _environment = environment;
         }
 
         #region Index
@@ -20,7 +27,59 @@ namespace IKIA.PL.Controllers
         {
             var departments = _departmentService.GetAllDepartments();
             return View(departments);
-        } 
+        }
+        #endregion
+        #region Create
+        #region Get
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        #endregion
+        #region Post
+        [HttpPost]
+        public IActionResult Create(CreatedDepartmentDTO department)
+        {
+            if (ModelState.IsValid)
+            {
+                var message = string.Empty;
+                try
+                {
+                    var result = _departmentService.CreateDepartment(department);
+                    if (result > 0)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        message = "Department was not created";
+                        ModelState.AddModelError(string.Empty,message);
+                        return View(department);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    //1- log exception
+                    _Logger.LogError(ex, ex.Message);
+                    //2- add error message to model state(set frindly message)
+                    if(_environment.IsDevelopment())
+                    {
+                       message = ex.Message;
+                       return View(department);
+
+                    }
+                    else
+                    {
+                        message = "Department was not created";
+                        return View("Error",message);
+                    }
+                }
+            }
+            return View(department);
+        }
+
+        #endregion
         #endregion
 
     }
